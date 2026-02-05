35871ce1f4d03bc4a069e1505318a225a9860207
Vedant Pankar <vedantpankar2@gmail.com>
2026-02-05 23:12:35 +0530

    Implement RightSidebar component with user details and media display
    - Added RightSidebar component to show selected user information including profile picture, name, and bio.
    - Integrated media display section with clickable images.
    - Updated HomePage layout to accommodate the new sidebar structure.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,7 +8,11 @@ const HomePage = () => {
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] ">
       <div
-        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-[1fr_2fr_1fr] relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-col-2"}`}
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid relative ${
+          selectedUser
+            ? "grid-cols-[1fr_2fr_1fr] md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "grid-cols-[1fr_2fr]"
+        }`}
       >
         <SideBar
           selectedUser={selectedUser}
